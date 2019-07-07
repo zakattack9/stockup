298e92b8712c11974ea507f544867aa13c2e2c7a
zakattack9 <zakattack26111@gmail.com>
2019-07-07 14:54:46 -0700

    added subtle hover animations to icons and articles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import StockArticles from './components/StockArticles';
 import StockData from './components/StockData';
+import icon from './assets/icon.png';
+import addIcon from './assets/addIcon.png'
 import './index.css';
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
     <div className="App">
       <StockData />
       <StockArticles />
+      <img src={icon} alt="Stockup Icon" className="stockupIcon"/>
+      <img src={addIcon} alt="Add Icon" className="addIcon"/>
     </div>
   );
 };
