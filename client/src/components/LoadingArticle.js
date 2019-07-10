63fc8ddd0186d90df012e738ab68fb945688ebde
zakattack9 <zakattack26111@gmail.com>
2019-07-09 21:17:35 -0700

    added ghost loading animation, moved filtering out articles without titles to backend which fixed fade in animation

---
+++ client/src/components/LoadingArticle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import './LoadingArticle.css';
+
+const Article = props => {
+  return (
+    <Fade left distance={'10px'}>
+      <div className="LoadingArticle">
+        <div className="loadingTitle" style={{ width: props.width }}>{props.title}</div>
+        <div className="loadingDate">{props.date}</div>
+      </div>
+    </Fade>
+  )
+};
+
+export default Article;
--- client/src/components/StockArticles.js
+++ client/src/components/StockArticles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Article from './Article';
+import LoadingArticle from './LoadingArticle';
 import './StockArticles.css';
 
 class StockArticles extends React.Component {
@@ -17,11 +18,12 @@ class StockArticles extends React.Component {
       let articlesToFade = 0;
       for (let article of allArticles) {
         let bounding = article.getBoundingClientRect();
-        console.log("ARTICLE BOUNDING", bounding.top)
-        if (bounding.top <= window.innerHeight) {
+        console.log(bounding.top)
+        if (bounding.top - 20 <= window.innerHeight) {
           articlesToFade++;
         }
       }
+      console.log(articlesToFade);
       this.setState({ articlesToFade })
     }
   }
@@ -38,7 +40,23 @@ class StockArticles extends React.Component {
     if (this.state.articleData === null) {
       return (
         <div className="StockArticles">
-          {/* Loading... */}
+          <div className="articlesWrapper">
+            <LoadingArticle width="70%" />
+            <LoadingArticle width="80%" />
+            <LoadingArticle width="65%" />
+            <LoadingArticle width="60%" />
+            <LoadingArticle width="70%" />
+            <LoadingArticle width="65%" />
+            <LoadingArticle width="75%" />
+            <LoadingArticle width="80%" />
+            <LoadingArticle width="75%" />
+            <LoadingArticle width="80%" />
+            <LoadingArticle width="65%" />
+            <LoadingArticle width="75%" />
+            <LoadingArticle width="80%" />
+            <LoadingArticle width="80%" />
+            <LoadingArticle width="80%" />
+          </div>
         </div>
       )
     }
@@ -47,17 +65,25 @@ class StockArticles extends React.Component {
       <div className="StockArticles">
         <div className="articlesWrapper">
           {this.state.articleData.map((article, i) => {
-            if (article.title === '') {
-              return false;
+
+            if (this.state.articlesToFade === 0) {
+              console.log(i, "adding initial articles");
+              return <Article delay={0} show={false} fade={false} key={i} title={article.title} date={article.date} link={article.link} />;
+            } else if (i + 1 <= this.state.articlesToFade && this.state.articlesToFade !== 0) {
+              console.log(i, "adding fade articles");
+              return <Article delay={(1 + i) * 70} show={true} fade={true} key={i} title={article.title} date={article.date} link={article.link} />;
+            } else if (i + 1 > this.state.articlesToFade && this.state.articlesToFade !== 0) {
+              console.log(i, "adding regular articles");
+              return <Article delay={0} show={true} fade={true} key={i} title={article.title} date={article.date} link={article.link} />;
             }
 
-            if (i + 1 <= this.state.articlesToFade) {
-              return <Article delay={i * 70} show={true} fade={true} key={i} title={article.title} date={article.date} link={article.link} />;
-            } else if (this.state.articlesToFade !== 0) {
-              return <Article delay={0} show={true} fade={true} key={i} title={article.title} date={article.date} link={article.link} />;
-            } else {
-              return <Article delay={0} show={false} fade={false} key={i} title={article.title} date={article.date} link={article.link} />;
-            }
+            // if (i + 1 <= this.state.articlesToFade && this.state.articlesToFade !== 0) {
+            //   return <Article delay={i * 70} show={true} fade={true} key={i} title={article.title} date={article.date} link={article.link} />;
+            // } else if (this.state.articlesToFade !== 0 && (i + 1) > this.state.articlesToFade) {
+            //   return <Article delay={0} show={true} fade={true} key={i} title={article.title} date={article.date} link={article.link} />;
+            // } else {
+            //   return <Article delay={0} show={false} fade={false} key={i} title={article.title} date={article.date} link={article.link} />;
+            // }
 
           })}
 
