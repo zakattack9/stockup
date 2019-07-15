39064374d5bb8bf2e9eb9646f562efd0be5410cd
zakattack9 <zakattack26111@gmail.com>
2019-07-15 07:46:59 -0700

    creds

--- client/src/api/creds.js
+++ client/src/api/creds.js
@@ -1,13 +1,6 @@
 // autobotguitar@gmail.com API Key
-<<<<<<< HEAD
 // const API_KEY = 'xChuFi5mSXppmC57wsykqNKv6qJm2MYQ5LYCsczhA1LrvDzexvIo8xrGXkJf';
 
 // zakattack26111@gmail.com API Key
 const API_KEY = 'JpSJgoj9s3dwSsEIZg8wqccTD6Tl81FpkmaGe3QiaOtWhCEtQPekT8s9c7fK';
-=======
-const API_KEY = 'xChuFi5mSXppmC57wsykqNKv6qJm2MYQ5LYCsczhA1LrvDzexvIo8xrGXkJf';
-
-// zakattack26111@gmail.com API Key
-// const API_KEY = 'JpSJgoj9s3dwSsEIZg8wqccTD6Tl81FpkmaGe3QiaOtWhCEtQPekT8s9c7fK';
->>>>>>> b2cd92bc585a5438dde936e6b5ab3e3ff4af39cd
 export default API_KEY;
